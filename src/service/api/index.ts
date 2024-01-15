f2701d4595d70e9e4f12253044a3ab5ee765079e
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2024-01-16 00:08:03 +0900

    Add content type in PokeApi

--- src/service/api/index.ts
+++ src/service/api/index.ts
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const PokeApi = axios.create({
   baseURL: 'https://pokeapi.co/api/v2',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default PokeApi;
