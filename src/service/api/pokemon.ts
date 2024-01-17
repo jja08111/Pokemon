ce8ad86c62d183e2fa3218f1ccfac4c54c9472ad
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2024-01-18 01:18:43 +0900

    Add return type in API

--- src/service/api/pokemon.ts
+++ src/service/api/pokemon.ts
@@ -4,7 +4,9 @@ import PokemonDetailResponse from '../model/PokemonDetailResponse';
 
 const PAGE_SIZE = 30;
 
-export const fetchPokemonList = async (page: number) => {
+export const fetchPokemonList = async (
+  page: number,
+): Promise<PokemonListResponse> => {
   const response = await PokeApi.get('/pokemon', {
     params: {
       offset: page * PAGE_SIZE,
@@ -14,7 +16,9 @@ export const fetchPokemonList = async (page: number) => {
   return PokemonListResponse.parse(response.data);
 };
 
-export const fetchPokemonDetail = async (pokemonName: string) => {
+export const fetchPokemonDetail = async (
+  pokemonName: string,
+): Promise<PokemonDetailResponse> => {
   const response = await PokeApi.get(`/pokemon/${pokemonName}`);
   return PokemonDetailResponse.parse(response.data);
 };
